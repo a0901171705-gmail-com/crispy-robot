f1245e9a62b72a126996dc9c38a3d66e36d6639a
孟晉 <a0901171705@gmail.com>
2025-06-22 04:36:33 +0800

    Initialize cli-9430

---
+++ internal/featuredetection/feature_detection_test.go
@@ -0,0 +1,450 @@
+package featuredetection
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/cli/cli/v2/internal/gh"
+	"github.com/cli/cli/v2/pkg/httpmock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIssueFeatures(t *testing.T) {
+	tests := []struct {
+		name          string
+		hostname      string
+		queryResponse map[string]string
+		wantFeatures  IssueFeatures
+		wantErr       bool
+	}{
+		{
+			name:     "github.com",
+			hostname: "github.com",
+			wantFeatures: IssueFeatures{
+				StateReason: true,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "ghec data residency (ghe.com)",
+			hostname: "stampname.ghe.com",
+			wantFeatures: IssueFeatures{
+				StateReason: true,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "GHE empty response",
+			hostname: "git.my.org",
+			queryResponse: map[string]string{
+				`query Issue_fields\b`: `{"data": {}}`,
+			},
+			wantFeatures: IssueFeatures{
+				StateReason: false,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "GHE has state reason field",
+			hostname: "git.my.org",
+			queryResponse: map[string]string{
+				`query Issue_fields\b`: heredoc.Doc(`
+					{ "data": { "Issue": { "fields": [
+						{"name": "stateReason"}
+					] } } }
+				`),
+			},
+			wantFeatures: IssueFeatures{
+				StateReason: true,
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reg := &httpmock.Registry{}
+			httpClient := &http.Client{}
+			httpmock.ReplaceTripper(httpClient, reg)
+			for query, resp := range tt.queryResponse {
+				reg.Register(httpmock.GraphQL(query), httpmock.StringResponse(resp))
+			}
+			detector := detector{host: tt.hostname, httpClient: httpClient}
+			gotFeatures, err := detector.IssueFeatures()
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantFeatures, gotFeatures)
+		})
+	}
+}
+
+func TestPullRequestFeatures(t *testing.T) {
+	tests := []struct {
+		name          string
+		hostname      string
+		queryResponse map[string]string
+		wantFeatures  PullRequestFeatures
+		wantErr       bool
+	}{
+		{
+			name:     "github.com with all features",
+			hostname: "github.com",
+			queryResponse: map[string]string{
+				`query PullRequest_fields\b`: heredoc.Doc(`
+				{
+					"data": {
+						"PullRequest": {
+							"fields": [
+								{"name": "isInMergeQueue"},
+								{"name": "isMergeQueueEnabled"}
+							]
+						},
+						"StatusCheckRollupContextConnection": {
+							"fields": [
+								{"name": "checkRunCount"},
+								{"name": "checkRunCountsByState"},
+								{"name": "statusContextCount"},
+								{"name": "statusContextCountsByState"}
+							]
+						}
+					}
+				}`),
+				`query PullRequest_fields2\b`: heredoc.Doc(`
+				{
+					"data": {
+						"WorkflowRun": {
+							"fields": [
+								{"name": "event"}
+							]
+						}
+					}
+				}`),
+			},
+			wantFeatures: PullRequestFeatures{
+				MergeQueue:                     true,
+				CheckRunAndStatusContextCounts: true,
+				CheckRunEvent:                  true,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "github.com with no merge queue",
+			hostname: "github.com",
+			queryResponse: map[string]string{
+				`query PullRequest_fields\b`: heredoc.Doc(`
+				{
+					"data": {
+						"PullRequest": {
+							"fields": []
+						},
+						"StatusCheckRollupContextConnection": {
+							"fields": [
+								{"name": "checkRunCount"},
+								{"name": "checkRunCountsByState"},
+								{"name": "statusContextCount"},
+								{"name": "statusContextCountsByState"}
+							]
+						}
+					}
+				}`),
+				`query PullRequest_fields2\b`: heredoc.Doc(`
+				{
+					"data": {
+						"WorkflowRun": {
+							"fields": [
+								{"name": "event"}
+							]
+						}
+					}
+				}`),
+			},
+			wantFeatures: PullRequestFeatures{
+				MergeQueue:                     false,
+				CheckRunAndStatusContextCounts: true,
+				CheckRunEvent:                  true,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "GHE with all features",
+			hostname: "git.my.org",
+			queryResponse: map[string]string{
+				`query PullRequest_fields\b`: heredoc.Doc(`
+				{
+					"data": {
+						"PullRequest": {
+							"fields": [
+								{"name": "isInMergeQueue"},
+								{"name": "isMergeQueueEnabled"}
+							]
+						},
+						"StatusCheckRollupContextConnection": {
+							"fields": [
+								{"name": "checkRunCount"},
+								{"name": "checkRunCountsByState"},
+								{"name": "statusContextCount"},
+								{"name": "statusContextCountsByState"}
+							]
+						}
+					}
+				}`),
+				`query PullRequest_fields2\b`: heredoc.Doc(`
+				{
+					"data": {
+						"WorkflowRun": {
+							"fields": [
+								{"name": "event"}
+							]
+						}
+					}
+				}`),
+			},
+			wantFeatures: PullRequestFeatures{
+				MergeQueue:                     true,
+				CheckRunAndStatusContextCounts: true,
+				CheckRunEvent:                  true,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "GHE with no features",
+			hostname: "git.my.org",
+			queryResponse: map[string]string{
+				`query PullRequest_fields\b`: heredoc.Doc(`
+				{
+					"data": {
+						"PullRequest": {
+							"fields": []
+						},
+						"StatusCheckRollupContextConnection": {
+							"fields": []
+						}
+					}
+				}`),
+				`query PullRequest_fields2\b`: heredoc.Doc(`
+				{
+					"data": {
+						"WorkflowRun": {
+							"fields": []
+						}
+					}
+				}`),
+			},
+			wantFeatures: PullRequestFeatures{
+				MergeQueue:                     false,
+				CheckRunAndStatusContextCounts: false,
+				CheckRunEvent:                  false,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reg := &httpmock.Registry{}
+			httpClient := &http.Client{}
+			httpmock.ReplaceTripper(httpClient, reg)
+			for query, resp := range tt.queryResponse {
+				reg.Register(httpmock.GraphQL(query), httpmock.StringResponse(resp))
+			}
+			detector := detector{host: tt.hostname, httpClient: httpClient}
+			gotFeatures, err := detector.PullRequestFeatures()
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantFeatures, gotFeatures)
+		})
+	}
+}
+
+func TestRepositoryFeatures(t *testing.T) {
+	tests := []struct {
+		name          string
+		hostname      string
+		queryResponse map[string]string
+		wantFeatures  RepositoryFeatures
+		wantErr       bool
+	}{
+		{
+			name:     "github.com",
+			hostname: "github.com",
+			wantFeatures: RepositoryFeatures{
+				PullRequestTemplateQuery: true,
+				VisibilityField:          true,
+				AutoMerge:                true,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "ghec data residency (ghe.com)",
+			hostname: "stampname.ghe.com",
+			wantFeatures: RepositoryFeatures{
+				PullRequestTemplateQuery: true,
+				VisibilityField:          true,
+				AutoMerge:                true,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "GHE empty response",
+			hostname: "git.my.org",
+			queryResponse: map[string]string{
+				`query Repository_fields\b`: `{"data": {}}`,
+			},
+			wantFeatures: RepositoryFeatures{
+				PullRequestTemplateQuery: false,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "GHE has pull request template query",
+			hostname: "git.my.org",
+			queryResponse: map[string]string{
+				`query Repository_fields\b`: heredoc.Doc(`
+					{ "data": { "Repository": { "fields": [
+						{"name": "pullRequestTemplates"}
+					] } } }
+				`),
+			},
+			wantFeatures: RepositoryFeatures{
+				PullRequestTemplateQuery: true,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "GHE has visibility field",
+			hostname: "git.my.org",
+			queryResponse: map[string]string{
+				`query Repository_fields\b`: heredoc.Doc(`
+					{ "data": { "Repository": { "fields": [
+						{"name": "visibility"}
+					] } } }
+				`),
+			},
+			wantFeatures: RepositoryFeatures{
+				VisibilityField: true,
+			},
+			wantErr: false,
+		},
+		{
+			name:     "GHE has automerge field",
+			hostname: "git.my.org",
+			queryResponse: map[string]string{
+				`query Repository_fields\b`: heredoc.Doc(`
+					{ "data": { "Repository": { "fields": [
+						{"name": "autoMergeAllowed"}
+					] } } }
+				`),
+			},
+			wantFeatures: RepositoryFeatures{
+				AutoMerge: true,
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reg := &httpmock.Registry{}
+			httpClient := &http.Client{}
+			httpmock.ReplaceTripper(httpClient, reg)
+			for query, resp := range tt.queryResponse {
+				reg.Register(httpmock.GraphQL(query), httpmock.StringResponse(resp))
+			}
+			detector := detector{host: tt.hostname, httpClient: httpClient}
+			gotFeatures, err := detector.RepositoryFeatures()
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantFeatures, gotFeatures)
+		})
+	}
+}
+
+func TestProjectV1SupportEnvOverride(t *testing.T) {
+	tt := []struct {
+		name            string
+		envVal          string
+		expectedSupport gh.ProjectsV1Support
+		expectedErr     bool
+	}{
+		{
+			name:            "PROJECTS_V1_SUPPORTED=true",
+			envVal:          "true",
+			expectedSupport: gh.ProjectsV1Supported,
+		},
+		{
+			name:            "PROJECTS_V1_SUPPORTED=false",
+			envVal:          "false",
+			expectedSupport: gh.ProjectsV1Unsupported,
+		},
+		{
+			name:        "PROJECTS_V1_SUPPORTED=nonboolean",
+			envVal:      "nonboolean",
+			expectedErr: true,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Setenv("PROJECTS_V1_SUPPORTED", tc.envVal)
+
+			detector := detector{}
+			support, err := detector.ProjectsV1()
+
+			if tc.expectedErr {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedSupport, support)
+		})
+	}
+}
+
+func TestProjectV1APINotSupported(t *testing.T) {
+	// Given the API does not include projects in the returned fields
+	reg := &httpmock.Registry{}
+	httpClient := &http.Client{}
+	httpmock.ReplaceTripper(httpClient, reg)
+	reg.Register(
+		httpmock.GraphQL(`query ProjectsV1FeatureDetection\b`),
+		httpmock.StringResponse(heredoc.Doc(`{ "data": { "Repository": { "fields": [] } } }`)),
+	)
+
+	// When we call the api to detect for ProjectV1 support
+	detector := detector{httpClient: httpClient}
+	actualSupport, err := detector.ProjectsV1()
+
+	// Then it succeeds and reports ProjectV1 is not supported
+	require.NoError(t, err)
+	require.Equal(t, gh.ProjectsV1Unsupported, actualSupport)
+}
+
+func TestProjectV1APISupported(t *testing.T) {
+	// Given the API includes projects in the returned fields
+	reg := &httpmock.Registry{}
+	httpClient := &http.Client{}
+	httpmock.ReplaceTripper(httpClient, reg)
+	reg.Register(
+		httpmock.GraphQL(`query ProjectsV1FeatureDetection\b`),
+		httpmock.StringResponse(heredoc.Doc(`{ "data": { "Repository": { "fields": [ {"name": "projects"} ] } } }`)),
+	)
+
+	// When we call the api to detect for ProjectV1 support
+	detector := detector{httpClient: httpClient}
+	actualSupport, err := detector.ProjectsV1()
+
+	// Then it succeeds and reports ProjectV1 is supported
+	require.NoError(t, err)
+	require.Equal(t, gh.ProjectsV1Supported, actualSupport)
+}
---
+++ pkg/cmd/issue/view/view.go
@@ -0,0 +1,174 @@
+package view
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/cli/cli/v2/api"
+	"github.com/cli/cli/v2/internal/browser"
+	fd "github.com/cli/cli/v2/internal/featuredetection"
+	"github.com/cli/cli/v2/internal/ghrepo"
+	"github.com/cli/cli/v2/internal/text"
+	issueShared "github.com/cli/cli/v2/pkg/cmd/issue/shared"
+	"github.com/cli/cli/v2/pkg/cmdutil"
+	"github.com/cli/cli/v2/pkg/iostreams"
+	"github.com/cli/cli/v2/pkg/set"
+	"github.com/spf13/cobra"
+)
+
+type ViewOptions struct {
+	HttpClient func() (*http.Client, error)
+	IO         *iostreams.IOStreams
+	BaseRepo   func() (ghrepo.Interface, error)
+	Browser    browser.Browser
+	Detector   fd.Detector
+
+	SelectorArg string
+	WebMode     bool
+	Comments    bool
+	Exporter    cmdutil.Exporter
+
+	Now func() time.Time
+
+	IssuePrinter IssuePrinter
+}
+
+type IssuePrinter interface {
+	Print(PresentationIssue, ghrepo.Interface) error
+}
+
+func NewCmdView(f *cmdutil.Factory, runF func(*ViewOptions) error) *cobra.Command {
+	opts := &ViewOptions{
+		IO:         f.IOStreams,
+		HttpClient: f.HttpClient,
+		Browser:    f.Browser,
+		Now:        time.Now,
+	}
+
+	cmd := &cobra.Command{
+		Use:   "view {<number> | <url>}",
+		Short: "View an issue",
+		Long: heredoc.Docf(`
+			Display the title, body, and other information about an issue.
+
+			With %[1]s--web%[1]s flag, open the issue in a web browser instead.
+		`, "`"),
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// support `-R, --repo` override
+			opts.BaseRepo = f.BaseRepo
+
+			if len(args) > 0 {
+				opts.SelectorArg = args[0]
+			}
+
+			if opts.IO.IsStdoutTTY() {
+				opts.IssuePrinter = &RichIssuePrinter{
+					IO:       opts.IO,
+					TimeNow:  opts.Now(),
+					Comments: opts.Comments,
+				}
+			} else {
+				opts.IssuePrinter = &RawIssuePrinter{
+					IO:       opts.IO,
+					Comments: opts.Comments,
+				}
+			}
+
+			if runF != nil {
+				return runF(opts)
+			}
+			return viewRun(opts)
+		},
+	}
+
+	cmd.Flags().BoolVarP(&opts.WebMode, "web", "w", false, "Open an issue in the browser")
+	cmd.Flags().BoolVarP(&opts.Comments, "comments", "c", false, "View issue comments")
+	cmdutil.AddJSONFlags(cmd, &opts.Exporter, api.IssueFields)
+
+	return cmd
+}
+
+var defaultFields = []string{
+	"number", "url", "state", "createdAt", "title", "body", "author", "milestone",
+	"assignees", "labels", "projectCards", "reactionGroups", "lastComment", "stateReason",
+}
+
+func viewRun(opts *ViewOptions) error {
+	httpClient, err := opts.HttpClient()
+	if err != nil {
+		return err
+	}
+
+	lookupFields := set.NewStringSet()
+	if opts.Exporter != nil {
+		lookupFields.AddValues(opts.Exporter.Fields())
+	} else if opts.WebMode {
+		lookupFields.Add("url")
+	} else {
+		lookupFields.AddValues(defaultFields)
+		if opts.Comments {
+			lookupFields.Add("comments")
+			lookupFields.Remove("lastComment")
+		}
+	}
+
+	opts.IO.DetectTerminalTheme()
+
+	opts.IO.StartProgressIndicator()
+	issue, baseRepo, err := findIssue(httpClient, opts.BaseRepo, opts.SelectorArg, lookupFields.ToSlice(), opts.Detector)
+	opts.IO.StopProgressIndicator()
+	if err != nil {
+		var loadErr *issueShared.PartialLoadError
+		if opts.Exporter == nil && errors.As(err, &loadErr) {
+			fmt.Fprintf(opts.IO.ErrOut, "warning: %s\n", loadErr.Error())
+		} else {
+			return err
+		}
+	}
+
+	if opts.WebMode {
+		openURL := issue.URL
+		if opts.IO.IsStdoutTTY() {
+			fmt.Fprintf(opts.IO.ErrOut, "Opening %s in your browser.\n", text.DisplayURL(openURL))
+		}
+		return opts.Browser.Browse(openURL)
+	}
+
+	if err := opts.IO.StartPager(); err != nil {
+		fmt.Fprintf(opts.IO.ErrOut, "error starting pager: %v\n", err)
+	}
+	defer opts.IO.StopPager()
+
+	if opts.Exporter != nil {
+		return opts.Exporter.Write(opts.IO, issue)
+	}
+
+	presentationIssue, err := MapApiIssueToPresentationIssue(issue, opts.IO.ColorScheme())
+	if err != nil {
+		return err
+	}
+
+	return opts.IssuePrinter.Print(presentationIssue, baseRepo)
+}
+
+func findIssue(client *http.Client, baseRepoFn func() (ghrepo.Interface, error), selector string, fields []string, detector fd.Detector) (*api.Issue, ghrepo.Interface, error) {
+	fieldSet := set.NewStringSet()
+	fieldSet.AddValues(fields)
+	fieldSet.Add("id")
+
+	issue, repo, err := issueShared.IssueFromArgWithFields(client, baseRepoFn, selector, fieldSet.ToSlice(), detector)
+	if err != nil {
+		return issue, repo, err
+	}
+
+	if fieldSet.Contains("comments") {
+		// FIXME: this re-fetches the comments connection even though the initial set of 100 were
+		// fetched in the previous request.
+		err = preloadIssueComments(client, repo, issue)
+	}
+	return issue, repo, err
+}
